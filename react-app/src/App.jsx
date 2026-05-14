73aeed383356dc246fdf6e96b463bc98fd20cf8b
Kirill <kirillshapoval0109@gmail.com>
2026-05-14 19:42:11 +0300

    Header-final

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,6 +1,4 @@
 import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Main from './components/Main/Main'
 import './App.css'
 
 function App() {
@@ -9,8 +7,6 @@ function App() {
     <>
       <div className="box">
         <Header />
-        <Main />
-        <Footer />
       </div>
     </>
   )
--- react-app/src/components/Header/Header.jsx
+++ react-app/src/components/Header/Header.jsx
@@ -1,5 +1,46 @@
-import 'Header.css'
+import './Header.css'
+import Search from '../../free-icon-search-4024513.png'
+import Header1 from '../../Group2.png'
+import Header2 from '../../Group3.png'
+import Header3 from '../../Group4.png'
+import Header4 from '../../Group5.png'
 
-export default function Header(){
-    <p>ABOBA</p>
+export default function Header() {
+    return (
+        <>
+            <header>
+                <nav>
+                    <h2>ЧАЙОВНЯ</h2>
+                    <div className="nav_buttons">
+                        <p>HOME</p>
+                        <p>TEA</p>
+                        <p>BAKERY</p>
+                        <p>SHOP</p>
+                        <p>ABOUT</p>
+                        <p>LOGIN</p>
+                    </div>
+                    <div className="nav_search">
+                        <img src={Search} />
+                    </div>
+                </nav>
+                <div className="header_txt">
+                    <h3>Welcome</h3>
+                    <h1>
+                        We serve the <br />
+                        richest tea in <br />
+                        the city!
+                    </h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing <br />
+                        elit, sed do eiusmod tempor</p>
+                    <button>Order Now</button>
+                </div>
+            </header>
+            <div className="under_header">
+                <img src={Header1} />
+                <img src={Header2} />
+                <img src={Header3} />
+                <img src={Header4} />
+            </div>
+        </>
+    )
 }
