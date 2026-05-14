41b112d5aec8dfdabb25fb7d65774c74748c1d58
Максик <maksimzitcenko@gmail.com>
2026-05-14 18:38:47 +0300

    База

---
+++ react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Main from './components/Main/Main'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="box">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
